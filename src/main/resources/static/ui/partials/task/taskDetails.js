067ed8ca3a2c499dcff2a21b5f7ba7de8e404501
MahdySoft <islamhaker@gmail.com>
2017-03-28 13:02:15 +0300

    Reports

--- src/main/resources/static/ui/partials/task/taskDetails.js
+++ src/main/resources/static/ui/partials/task/taskDetails.js
@@ -1,10 +1,16 @@
-app.controller('taskDetailsCtrl', ['ModalProvider', '$scope', '$rootScope', '$timeout', '$log', '$uibModalInstance', 'task',
-    function (ModalProvider, $scope, $rootScope, $timeout, $log, $uibModalInstance, task) {
+app.controller('taskDetailsCtrl', ['ModalProvider', 'TaskOperationService', '$scope', '$rootScope', '$timeout', '$log', '$uibModalInstance', 'task',
+    function (ModalProvider, TaskOperationService, $scope, $rootScope, $timeout, $log, $uibModalInstance, task) {
 
         $scope.task = task;
 
         $scope.openCreateOperationModel = function () {
             ModalProvider.openTaskOperationCreateModel($scope.task);
+        };
+
+        $scope.findTaskOperations = function () {
+            TaskOperationService.findByTask($scope.selected).then(function (data) {
+                $scope.task.taskOperations = data;
+            })
         };
 
         $scope.cancel = function () {
