9126d4dbab2344858363a5b32076d973cd17f3fc
MahdySoft <islamhaker@gmail.com>
2017-04-14 15:25:42 +0300

    Deploy

--- src/main/java/com/besafx/app/component/ScheduledTasks.java
+++ src/main/java/com/besafx/app/component/ScheduledTasks.java
@@ -192,7 +192,7 @@ public class ScheduledTasks {
         emailSender.send(title, message, to.getEmail());
     }
 
-    @Scheduled(cron = "0 0 9 * * *")
+    @Scheduled(cron = "0 0 9 * * SUN,MON,TUE,WED,THU")
     public void sendReportAboutTaskTosCheck() throws InterruptedException, IOException, JRException, ExecutionException {
         Iterator<Person> iterator = personService.findAll().iterator();
         while (iterator.hasNext()) {
